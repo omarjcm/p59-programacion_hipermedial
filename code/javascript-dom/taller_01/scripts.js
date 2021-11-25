5fc9bbe8d54563aaa229367ffd0925d5e03ade0b
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-11-24 20:46:13 -0500

    DOM - Cargar Elemento

---
+++ code/javascript-dom/taller_01/scripts.js
@@ -0,0 +1,8 @@
+function cargarElemento() {
+    var h1 = document.createElement('h1')
+    var texto = document.createTextNode('DOM - Document Object Model')
+    h1.appendChild( texto )
+
+    var container = document.getElementById('container')
+    container.appendChild( h1 )
+}
