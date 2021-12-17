2ae75c2210d45fc7884c0e8574f7df3da3e2efce
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-12-16 19:04:58 -0500

    Creacion del modulo empleado, creacion del modelo, sincronizacion con BD, adicion al admin.

---
+++ code/proyecto/tiendita/empleado/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from empleado.models import Empleado
+
+# Register your models here.
+admin.site.register( Empleado )
---
+++ code/proyecto/tiendita/empleado/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+class Empleado(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    url = models.URLField(max_length=300, blank=True)
+    biografia = models.TextField(blank=True)
+    numero_telefono = CharField(max_length=20, blank=True)
+    picture = models.ImageField(
+        upload_to='empleado/imagenes',
+        blank=True,
+        null=True
+    )
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
