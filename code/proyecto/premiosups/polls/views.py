273649a4953c58aff0a2121f84aec44e68bb8669
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-27 19:07:02 -0500

    Implementacion de la vista y la plantilla para visualizar las preguntas.

--- code/proyecto/premiosups/polls/views.py
+++ code/proyecto/premiosups/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola mundo')
+    questions = Question.objects.all()
+    return render(request, 'polls/index.html', { 'latest_question_list': questions })
+
+def detail(request, question_id):
+    return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando a la pregunta numero {question_id}')
