0c203ae55545a109ead9933a0b1cfca2dbea1967
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2022-01-27 19:25:36 -0500

    Implementacion de URL hard-coding

--- code/proyecto/premiosups/polls/views.py
+++ code/proyecto/premiosups/polls/views.py
@@ -9,7 +9,8 @@ def index(request):
     return render(request, 'polls/index.html', { 'latest_question_list': questions })
 
 def detail(request, question_id):
-    return HttpResponse(f'Estas viendo la pregunta numero {question_id}')
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', { 'question': question})
 
 def results(request, question_id):
     return HttpResponse(f'Estas viendo los resultados de la pregunta numero {question_id}')
